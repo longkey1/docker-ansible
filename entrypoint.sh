59df575a637abf809a1f8690813860c671831550
longkey1 <longkey1@gmail.com>
2025-05-04 10:47:23 +0900

    feat: add user creation with UID/GID specification
    - Add a local user with specified UID and GID.
    - Add a warning message if the user already exists with a different UID/GID.
    - Change the echo message to show the UID/GID of the user.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
+set -e
 
-USER_ID=${LOCAL_UID:-9001}
-GROUP_ID=${LOCAL_GID:-9001}
+# Add local user
+# Either use the LOCAL_USER_ID if passed in at runtime or
+# fallback
+USER_ID=${LOCAL_USER_ID:-9001}
+GROUP_ID=${LOCAL_GOURP_ID:-9001}
 QUIET_MSG=${QUIET_MSG:-FALSE}
 
-if [ "${QUIET_MSG}" = "FALSE" ]; then
-  echo "Starting with UID : ${USER_ID}, GID: ${GROUP_ID}"
-  useradd -u ${USER_ID} -o -d ${WORK_DIR} worker
-  groupmod -g ${GROUP_ID} worker
-else
-  useradd -u ${USER_ID} -o -d ${WORK_DIR} worker > /dev/null 2>&1
-  groupmod -g ${GROUP_ID} worker > /dev/null 2>&1
-fi
-export HOME=/work
+getent group user > /dev/null 2>&1 || groupadd -g $GROUP_ID worker
+id -u worker > /dev/null 2>&1 || useradd --shell /bin/bash -u $USER_ID -g $GROUP_ID -o -c "" -m worker
 
-chown worker:worker ${WORK_DIR}
+LOCAL_UID=$(id -u worker)
+LOCAL_GID=$(getent group worker | cut -d ":" -f 3)
+
+if [ ! "$USER_ID" == "$LOCAL_UID" ] || [ ! "$GROUP_ID" == "$LOCAL_GID" ]; then
+    echo "Warning: User with differing UID "$LOCAL_UID"/GID "$LOCAL_GID" already exists, most likely this container was started before with a different UID/GID. Re-create it to change UID/GID."
+fi
+
+if [ "${QUIET_MSG}" = "FALSE" ]; then
+  echo "Starting with UID/GID : "$(id -u user)"/"$(getent group user | cut -d ":" -f 3)
+fi
+
+export HOME=/work
 
 exec /usr/sbin/gosu worker "$@"
